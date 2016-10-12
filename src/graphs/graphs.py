9ee737ac99a48c8a09d1452396a3a8b5eafb1d79
Livanux <ivangastelumtf@gmail.com>
2016-10-11 22:37:29 -0700

    Python version completed

--- src/graphs/graphs.py
+++ src/graphs/graphs.py
@@ -6,6 +6,10 @@
 # Author: Jesús Iván Gastelum Romero              
 # Contributors: Luis Edmundo Espinoza Larios     
 
+from time import time
+start_time = time()
+
+
 graph = [
         [1, 2, 4, 8],
         [2, 1, 3, 4],
@@ -24,60 +28,44 @@ SIZE = len(graph)
 def goTo(graph, start, end):
 
 	visited = [0] * len(graph)
-
 	startIndex = getIndex(graph, start)
-
 	visited[startIndex] = 1
-
-
-	print visited
-
 
 	dfs(graph, visited, start, end)
 
-	print '\n'
-
-
 def  dfs(graph, visited, current, end):
-
 	currentIndex = getIndex(graph, current)
 
-	print  '->', graph[currentIndex][0],
+	print  graph[currentIndex][0], '->', 
 
 	if current == end:
 		return 1
 
 	visited[currentIndex] = 1
 
-	i= 0
-	print visited
-
-	while i < SIZE and graph[currentIndex][i] != 0:
-
-		print '\n', i, graph[currentIndex][i]
-
+	for  i in range(len(graph[currentIndex])):
 		visitedIndex = getIndex(graph, graph[currentIndex][i])
 
 		if (not visited[visitedIndex]):
+
 			if (dfs(graph, visited, graph[currentIndex][i], end)):
-				return 1
-
+				return True
 		i += 1
-	return 0
-
+	return False
 
 
 def getIndex(graph, number):
-
 	i = 0
-
 	while i < SIZE:
 		if (graph[i][0] == number):
 			return i
 		i += 1
-
-	return (i-1)
+	return -1
 
 #---------MAIN----------#
 
 goTo(graph, 9, 8)
+
+
+end_time = time() - start_time
+print '\nExecution Time: ', end_time, '\n'
